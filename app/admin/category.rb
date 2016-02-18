f6100cc9e8970b3e0f9ecce9abb2baa35395d2ef
amyalenkov <stmy999@gmail.com>
2016-02-18 10:29:56 +0300

    active admin: filters for models.

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,5 +1,9 @@
 ActiveAdmin.register Category do
 
+  config.per_page = 30
+
+  filter :name
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
--- app/admin/product_picture.rb
+++ app/admin/product_picture.rb
@@ -1,17 +1,7 @@
 ActiveAdmin.register ProductPicture do
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
+  config.per_page = 30
 
+  filter :url
 
 end
