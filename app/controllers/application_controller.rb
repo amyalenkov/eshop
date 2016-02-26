da62e7d65e68ea37b86b147ad7d21ce028282896
dsuschinsky <dsuschinsky@gmail.com>
2016-02-26 16:15:29 +0300

    edit mark for pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :set_menu
+  before_filter :get_submenu_by_categorie
 
   private
 
   def set_menu
     @categories = Category.includes :subcategories
   end
+
+  # def get_submenu_by_categorie
+  #   @subcategories = Subcategory.find_by_category_id @categories
+  # end
 end
