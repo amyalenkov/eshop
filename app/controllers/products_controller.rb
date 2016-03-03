928badf59ef8656eac2244cd942d6d3c2cfd24cd
amyalenkov <stmy999@gmail.com>
2016-03-03 20:16:12 +0300

    products for cats and subcats.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,13 +3,18 @@ class ProductsController < ApplicationController
   def index
     if request.url.to_s.include? '/category/'
       category = Category.find_by_name params[:name]
-      @subcategories = Subcategory.where :category_id => category.id
-      @products = Product.includes(:subcategory).joins(:product_pictures).references(@subcategories).page(params[:page])
+      all_subcategories,@subcategories = get_subs(category.id)
+      @products = Product.includes(:subcategory).joins(:product_pictures).references(all_subcategories).
+          where(subcategory_id: all_subcategories).page(params[:page])
     elsif request.url.to_s.include? '/subcategory/'
       subcategory = Subcategory.find_by_name params[:name]
-      @subcategories = Subcategory.where :category_id => subcategory.id
-      @products = Product.includes(:subcategory).joins(:product_pictures).
-          references(@subcategories).page(params[:page])
+      # @subcategories = Subcategory.where :category_id => subcategory.id
+      all_subcategories,@subcategories = get_subs(subcategory.id)
+      all_subcategories.push subcategory
+      @products = Product.includes(:subcategory).joins(:product_pictures).references(all_subcategories).
+          where(subcategory_id: all_subcategories).page(params[:page])
+      p @products.size
+      p Product.all.size
     else
       @products = Product.all.page(params[:page])
     end
@@ -28,6 +33,29 @@ class ProductsController < ApplicationController
   def search
     @search_products  = ThinkingSphinx.search params[:search], classes: [Product], :star => true,
                                               :page => params[:page], :per_page => 25
-
   end
+
+  private
+
+  def get_subs id
+    arr = Array.new
+    get_subs_requrs id, arr
+  end
+
+  def get_subs_requrs id, arr
+    new_arr = get_subs_by_id id
+    first_arr = new_arr
+    unless new_arr.size == 0
+      (arr << new_arr).flatten!
+      new_arr.each do |sub|
+        get_subs_requrs sub.id, arr
+      end
+    end
+    return arr, first_arr
+  end
+
+  def get_subs_by_id id
+    Subcategory.where :category_id => id
+  end
+
 end
