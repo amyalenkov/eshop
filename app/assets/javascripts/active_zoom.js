fc811b8b31e201b4416ed0d16bf0314e63922f43
sushi <dsuschinsky@gmail.com>
2016-03-06 13:01:07 -0800

    add zoomer

--- app/assets/javascripts/active_zoom.js
+++ app/assets/javascripts/active_zoom.js
@@ -1,14 +1,10 @@
-//$("#zoom_image").elevateZoom({gallery:'gallery_01', cursor: 'pointer', galleryActiveClass: 'active', imageCrossfade: true,
-//    loadingIcon: 'http://www.elevateweb.co.uk/spinner.gif'});
-
 $(document).ready(function() {
     $("#zoom_image").elevateZoom({gallery: 'gallery_01', cursor: 'pointer', galleryActiveClass: 'active'});
-
-    $("#gal1").bind("click", function (e) {
-        var elem = $.find
-        console.log("click");
-        var ez = $('#zoom_image').data('elevateZoom');
-        $.fancybox(ez.getGalleryList());
-        return false;
-    });
 });
+
+function get_id(clicked_id)
+{
+    var new_src = document.getElementById(clicked_id).src;
+    document.getElementById("zoom_image").setAttribute("src", new_src);
+    return false;
+}
