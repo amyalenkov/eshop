7013a69da72580cf800f84e6bf8250e822669245
sushi <dsuschinsky@gmail.com>
2016-03-11 13:51:41 -0800

    add schema to gitIgnore

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308121725) do
+ActiveRecord::Schema.define(version: 20160309205804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20160308121725) do
     t.integer  "product_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "product_picture"
   end
 
   add_index "cart_items", ["product_id"], name: "index_cart_items_on_product_id", using: :btree
