633852719246b22aecf72a9bf8dba229c9849e37
amyalenkov <stmy999@gmail.com>
2016-03-17 16:15:29 +0300

    states for bill

--- app/models/row.rb
+++ app/models/row.rb
@@ -15,15 +15,8 @@ class Row < ActiveRecord::Base
     p 'check_state'
     if current_count >= min_count
       self.state = Row.states[:full]
-      self.row_items.each do |row_item|
-        row_item.order_item.reserving!
-      end
     else
       self.state = Row.states[:not_full]
-      self.row_items.each do |row_item|
-        order_item = row_item.order_item
-        order_item.in_progress! unless order_item.nil?
-      end
     end
   end
 
