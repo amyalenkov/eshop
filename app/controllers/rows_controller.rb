a1addc476317d586c7365056997a6100ecf3dd07
amyalenkov <stmy999@gmail.com>
2016-03-18 14:44:30 +0300

    counter for order: disabled, check in controller.

--- app/controllers/rows_controller.rb
+++ app/controllers/rows_controller.rb
@@ -27,18 +27,20 @@ class RowsController < ApplicationController
   end
 
   def update
+    count = params[:count].to_i
     row_item = RowItem.find_by row_id: params[:row_id], user_id: current_user.id
-    old_row_count = row_item.count
-    row_item.count = params[:count].to_i
-    row_item.save!
     @order_item = row_item.order_item
-    @order_item.count = row_item.count
-    @order_item.save!
     @row = Row.find_by_id params[:row_id]
-    @row.current_count = @row.current_count - old_row_count + params[:count].to_i
-    @row.check_state
-    @row.save!
-    # redirect_to @row
+    if count > 0
+      old_row_count = row_item.count
+      row_item.count = count
+      row_item.save!
+      @order_item.count = row_item.count
+      @order_item.save!
+      @row.current_count = @row.current_count - old_row_count + count
+      @row.check_state
+      @row.save!
+    end
   end
 
   def destroy
