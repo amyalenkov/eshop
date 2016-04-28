ca7131637f93e2ee0d527830ad69eb7b925bbc41
dsuschinsky <dsuschinsky@gmail.com>
2016-04-28 16:31:37 +0300

    edit product info for products/index

--- app/admin/calendar_dates.rb
+++ app/admin/calendar_dates.rb
@@ -1,5 +1,14 @@
 ActiveAdmin.register CalendarDate do
 
+  permit_params :name, :start_time
+
   config.per_page = 30
 
+  form do |f|
+    f.inputs 'Edit date' do
+      f.input :name
+      f.input :start_time, as: 'Just_datetime_picker'
+    end
+    f.actions
+  end
 end
