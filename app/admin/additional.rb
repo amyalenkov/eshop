909e9ca73b7c31cbf8d93acd941576885f93d3bf
amyalenkov <stmy999@gmail.com>
2016-05-11 12:10:30 +0300

    admin panel: start translating.

--- app/admin/additional.rb
+++ app/admin/additional.rb
@@ -1,7 +1,17 @@
 ActiveAdmin.register Additional do
 
+  menu label: 'Конфигурация'
+
+  index :title => 'Конфигурация' do
+    selectable_column
+    column 'Название', :name
+    column 'Значение', :value
+    actions
+  end
+
+
   config.per_page = 30
 
-  filter :name
+  filter :name, label: 'Название'
 
 end
--- app/admin/configure.rb
+++ app/admin/configure.rb
@@ -4,15 +4,28 @@ ActiveAdmin.register Configure do
 
   config.per_page = 30
 
+
+  menu label: 'Настройка стопа и оплаты'
+
+  index :title => 'Настройка стопа и оплаты' do
+    selectable_column
+    column 'Название', :name
+    column 'День недели', :day_of_week
+    column 'Время', :time
+    actions
+  end
+
   form do |f|
-    f.inputs 'Edit Main Order' do
-      f.input :name
+    f.inputs 'Редактирование' do
+      # f.input :name
       f.input :day_of_week, as: :select, collection: Configure.day_of_weeks.keys.to_a
       f.input :time
-      f.input :value
+      # f.input :value
     end
     f.actions
   end
+
+  filter :name, label: 'Название'
 
 
 end
