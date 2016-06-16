62a696437558b6716966984fe19caf1c77037011
dsuschinsky <dsuschinsky@gmail.com>
2016-06-16 10:45:51 +0300

    sortBy.js

--- app/assets/javascripts/sort_by.js
+++ app/assets/javascripts/sort_by.js
@@ -1,7 +1,58 @@
 function sortBy(typeSort){
-    document.cookie = "sort_by="+typeSort;
-    console.log(typeSort);
+    deleteAllCookies();
+    setCookie('sort_by', typeSort, {expires: 0});
+    console.log('typeSort: '+typeSort);
     if (typeSort=="by_line"){
-        document.cookie = "class_for_sort_by=product_in_line";
+        setCookie('class_for_sort_by', 'product_in_line', {expires: 0});
+    }
+    else{
+        deleteCookie('class_for_sort_by');
+    }
+    //alert(document.cookie);
+}
+
+function setCookie(name, value, options) {
+    options = options || {};
+
+    var expires = options.expires;
+
+    if (typeof expires == "number" && expires) {
+        var d = new Date();
+        d.setTime(d.getTime() + expires * 1000);
+        expires = options.expires = d;
+    }
+    if (expires && expires.toUTCString) {
+        options.expires = expires.toUTCString();
+    }
+
+    value = encodeURIComponent(value);
+
+    var updatedCookie = name + "=" + value;
+
+    for (var propName in options) {
+        updatedCookie += "; " + propName;
+        var propValue = options[propName];
+        if (propValue !== true) {
+            updatedCookie += "=" + propValue;
+        }
+    }
+
+    document.cookie = updatedCookie;
+}
+
+function deleteCookie(name) {
+    setCookie(name, "", {
+        expires: -1
+    })
+}
+
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     }
 }
