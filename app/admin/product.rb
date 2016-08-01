f51a7925526ec04598e316c0a32d04e5b12cd3e0
dsuschinsky <dsuschinsky@gmail.com>
2016-08-01 09:38:17 +0300

    edit product table

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -6,4 +6,32 @@ ActiveAdmin.register Product do
 
   filter :name
 
+  permit_params :name, :title, :description
+
+  config.per_page = 30
+
+  form do |f|
+    f.inputs 'Edit' do
+      f.input :name
+      f.input :description
+      f.input :depth
+      f.input :width
+      f.input :height
+      f.input :sid
+      f.input :k_min, as: :check_boxes
+      f.input :is_hit, as: :check_boxes
+      f.input :min_qty
+      f.input :balance_text
+      f.input :box_size_text
+      f.input :materials_text
+      f.input :size_text
+      f.input :unit
+      f.input :image
+      f.input :price
+      f.input :weight
+      f.input :certificate_type
+      f.input :box_type
+    end
+    f.actions
+  end
 end
