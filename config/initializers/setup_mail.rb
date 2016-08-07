618586c50c4878ede29899fa50a8601fe874eb9c
dsuschinsky <dsuschinsky@gmail.com>
2016-08-08 00:50:04 +0300

    edit email

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,10 +2,10 @@ ActionMailer::Base.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     domain: 'uzelok4you.by',
-    # user_name: 'dsuschinsky@gmail.com',
-    user_name: 'uzelok4you@gmail.com',
-    # password: 'xcbk44vU',
-    password: 'youruzelok',
+    user_name: 'dsuschinsky@gmail.com',
+    # user_name: 'uzelok4you@gmail.com',
+    password: 'xcbk44vU',
+    # password: 'youruzelok',
     authentication: 'plain',
     enable_starttls_auto: true
 }
