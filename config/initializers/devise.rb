e512e3338a5c842e184ad4d1aa8fdaa181740fe4
dsuschinsky <dsuschinsky@gmail.com>
2016-08-08 00:46:49 +0300

    edit email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'root@uzelok4you.by'
+  # config.mailer_sender = 'root@uzelok4you.by'
+  config.mailer_sender = 'uzelok4you@gmail.com'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'
