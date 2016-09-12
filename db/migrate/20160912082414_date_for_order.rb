69ca2df072f73ee2a0660904e0aa725e02746fb1
dsuschinsky <dsuschinsky@gmail.com>
2016-09-12 12:02:39 +0300

    date for order

---
+++ db/migrate/20160912082414_date_for_order.rb
@@ -0,0 +1,9 @@
+class DateForOrder < ActiveRecord::Migration
+  def change
+    create_table :date_for_orders do |t|
+      t.text :name
+      t.text :date
+      t.text :description
+    end
+  end
+end
