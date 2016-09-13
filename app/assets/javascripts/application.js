0f3d9d0065eab8fb712b855aea8ae3e8763ffbad
dsuschinsky <dsuschinsky@gmail.com>
2016-09-13 21:17:50 +0300

    for users FAQ

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,7 +34,7 @@
 //= require products
 //= require slider_content
 //= require downTop
-//= require count_down_timer
+/*= require count_down_timer*/
 //= require jivosite
 //= require sort_by
 //= require jquery.inputmask
