930f5f53bd7fc2bc1ec6f6f0944843abd8d05562
dsuschinsky <dsuschinsky@gmail.com>
2016-09-21 17:00:19 +0300

    post_loader

--- app/assets/javascripts/post_loader.js
+++ app/assets/javascripts/post_loader.js
@@ -12,3 +12,25 @@ $(function(){
     setTimeout(function(){
         changeAttr()}, 2500);
 });
+
+$(function(){
+    var changeCategoryLogo = function(){
+        $('#catalog_items div img[class]').each(function(){
+            $(this).attr('src', $(this).attr('class')).removeAttr('class')
+        })
+    };
+
+    setTimeout(function(){
+        changeCategoryLogo()}, 1500);
+});
+
+//$(function(){
+//    var changeSubCategoryLogo = function(){
+//        $('#list_categories div img[class]').each(function(){
+//            $(this).attr('src', $(this).attr('class')).removeAttr('class')
+//        })
+//    };
+//
+//    setTimeout(function(){
+//        changeSubCategoryLogo()}, 1500);
+//});
