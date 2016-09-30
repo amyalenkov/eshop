c05b0c60e756abb968ccc2023039a5758b9f7483
dsuschinsky <dsuschinsky@gmail.com>
2016-09-30 15:32:26 +0300

    update user status message

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,2 +1,12 @@
 class Reminder < ActiveRecord::Base
+
+  after_commit :update_user_message
+
+  def update_user_message
+    User.where(choice_delivery: true).update_all(choice_delivery: false)
+    User.where(after_stop: true).update_all(after_stop: false)
+    User.where(info_stop: true).update_all(info_stop: false)
+    User.where(info_delivery: true).update_all(info_delivery: false)
+  end
+
 end
