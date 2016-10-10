d8f6ca3370dfd9222485e2bea6fdd1b6dc30ebaa
dsuschinsky <dsuschinsky@gmail.com>
2016-10-10 12:19:57 +0300

    default count for add product in cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -12,13 +12,23 @@ class CartItemsController < ApplicationController
     product_id = params[:cart_item][:product_id]
     count = params[:cart_item][:count]
     unless current_user.product_in_cart? product_id
-      @product = Product.find_by_id product_id
-      cart_item = CartItem.new
-      cart_item.product_id = @product.id
-      cart_item.count = count
-      cart_item.total_price = count * @product.get_price_int
-      cart_item.user_id = current_user.id
-      cart_item.save!
+      if count == 0
+        @product = Product.find_by_id product_id
+        cart_item = CartItem.new
+        cart_item.product_id = @product.id
+        cart_item.count = 1
+        cart_item.total_price = count * @product.get_price_int
+        cart_item.user_id = current_user.id
+        cart_item.save!
+      else
+        @product = Product.find_by_id product_id
+        cart_item = CartItem.new
+        cart_item.product_id = @product.id
+        cart_item.count = count
+        cart_item.total_price = count * @product.get_price_int
+        cart_item.user_id = current_user.id
+        cart_item.save!
+      end
     end
   end
 
