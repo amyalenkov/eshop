fd401273807d33f5cb20a1079e25dbd998da6702
amyalenkov <stmy999@gmail.com>
2016-11-16 14:20:51 +0300

    remake cart_item in admin panel.

--- app/admin/cart_item.rb
+++ app/admin/cart_item.rb
@@ -6,4 +6,24 @@ ActiveAdmin.register CartItem do
 
   filter :user, label: 'Юзер'
 
+  index do
+    column :id
+    column :count
+    column :user_name do |cart_item|
+      cart_item.user.name
+    end
+    column :phone_number do |cart_item|
+      cart_item.user.phone
+    end
+    column :user_email do |cart_item|
+      cart_item.user.email
+    end
+    column :product_id
+    column :comment
+    # column "Price", sortable: :price do |product|
+    #   number_to_currency product.price
+    # end
+    actions
+  end
+
 end
